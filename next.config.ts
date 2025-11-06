732f3f57ffd4a7f7812e94bfa518e628deb428ce
Rahma Sari <23502310024@student.prasetiyamulya.ac.id>
2025-11-07 00:05:39 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uts-20251-it-fin-tech-rahma-sari.vercel.app",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
